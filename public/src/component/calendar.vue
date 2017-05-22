f05a3726c6790cbb8e256ffd1717e3569cdfe172
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-05-22 18:28:06 +0800

    vue 基本路由配置

--- public/src/component/calendar.vue
+++ public/src/component/calendar.vue
@@ -1,6 +1,50 @@
 <template id="calendar">
 	<div >
-		<h2>我的</h2>
+		<div class="banner">
+			<img src="../images/banner.jpg"/>
+		</div>
+		<!--banner end -->
+		<!--<div class="bg_box" hidden="{{flag}}" bindtap="bg_box">
+		<swiper class="swiper_box" hidden="{{flag}}">
+		  <block wx:for="{{imgUrls}}">
+		    <swiper-item>
+		      <img src="{{item}}" class="slide-img"/>
+		      <div class="clickBox_time" >
+				<text>4月13日</text><text>星期四</text><text>08:20:40</text>
+				<div class="clickBox_bottom">今天很高兴今天很高兴今天很高兴今天很高兴今天很高兴</div>
+			  </div>
+		    </swiper-item>
+		  </block>
+		</swiper>
+		</div>
+		<div class="rl_header">
+		    <div class="canlendarBgdiv">
+		      <div class="canlendardiv">
+		        <div class="canlendarTopdiv">
+		          <div class="leftBgdiv" bindtap="handleCalendar" data-handle="prev">
+		            <img class="old" src="../imgs/back.png"/>
+		          </div>
+		          <div class="centerdiv">{{cur_year || "--"}} 年 {{cur_month || "--"}} 月</div>
+		          <div class="rightBgdiv" bindtap="handleCalendar" data-handle="next">
+		            <img class="next" src="../imgs/back.png"/>
+		          </div>
+		      </div>
+		      <div class="weekBgdiv">
+		        <div class="weekdiv" wx:for="{{weeks_ch}}" wx:key="{{index}}" data-idx="{{index}}">{{item}}       </div>
+		      </div>
+		      <div class="dateBgdiv">
+		        <div wx:if="{{hasEmptyGrid}}" class="dateEmptydiv" wx:for="{{empytGrids}}" wx:key="{{index}}" data-idx="{{index}}">
+		        </div>
+		        <div class="datediv {{_month == cur_month&&index == today-1 ? 'dateSelectdiv' : ''}}" wx:for="{{days}}" wx:key="{{index}}" data-idx="{{index}}" bindtap="swiper_box">
+		        
+		          <div class="datesdiv">{{item}}</div>
+		          <img src="../imgs/rili_xl.png"/>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+		</div>		-->
+
 	</div>
 </template>
 
@@ -12,4 +56,11 @@
 </script>
 
 <style>
+	.banner{
+	  width: 100%;
+	}
+	.banner img{
+		display: block;
+		width: 100%;
+	}
 </style>
--- public/src/component/myCenter.vue
+++ public/src/component/myCenter.vue
@@ -3,7 +3,10 @@
 		<div class="banner">
 			<img src="../images/banner.jpg"/>
 		</div>
+		<!--banner end -->
+		
 		<div class="mycenter_list">
+			<!--mycenter start-->
 			<div class="mycenter">
 				<a href="">
 					<div class="list_left">
@@ -15,20 +18,50 @@
 					</div>
 				</a>
 			</div>
+			<!--mycenter end-->
+			
 			<div class="mycenterFill"></div>
-			<div class="mycenter">
+			
+			<!--friendcenter start-->
+			<div class="mycenter friendCenter">
 				<a href="">
 					<div class="list_left">
-						<img class="headerimg" src="../images/13.jpg"/>
-						<span>欧阳</span>
+						<img class="headerimg" src="../images/34.jpg"/>
+						<div class="friend">
+							<p class="friendName">丫丫</p>
+							<p class="time">11:00</p>
+						</div>
 					</div>
 					<div class="list_right">
-						
+						<img class="moodimg" src="../images/list_mood_07.png"/>
+						<div class="interaction">
+							<div>0</div>
+							<img src="../images/list_dianz_nor.png" alt="" />
+						</div>
 					</div>
 				</a>
 			</div>
-			
-		</div>
+			<div class="mycenter friendCenter">
+				<a href="">
+					<div class="list_left">
+						<img class="headerimg" src="../images/34.jpg"/>
+						<div class="friend">
+							<p class="friendName">丫丫</p>
+							<p class="time">11:00</p>
+						</div>
+					</div>
+					<div class="list_right">
+						<img class="moodimg" src="../images/list_mood_07.png"/>
+						<div class="interaction">
+							<div>5</div>
+							<img src="../images/list_dianz_pre.png" alt="" />
+						</div>
+					</div>
+				</a>
+			</div>
+			<a class="share" href="">朋友太少，点击生成邀请卡，分享到朋友圈</a>
+		</div>  
+        <!--friendcenter end-->     
 		
 	</div>
 </template>
@@ -57,20 +90,30 @@ module.exports = myCenter
 		padding:0 20px;
 	}
 	.mycenter_list{
-		
 		height: 100%;
-		
-		
 	}
 	.mycenter a{
 		height: 72px;
 		display: block;
-		
-		
+	}
+	.friend{
+		margin-left: 55px;
+		padding-top: 28px;
+	}
+	.friendName{
+		font-size: 16px;
+		color: #000000;
+		line-height: 0;
+		margin-bottom: 11px;
+	}
+	.time{
+		font-size: 12px;
+		color: #999999;
 	}
 	.list_left{
 		float: left;
 		position: relative;
+		height: 100%;
 	}
 	.headerimg{
 		height: 40px;
@@ -89,16 +132,50 @@ module.exports = myCenter
 	}
 	.list_right{
 		float: right;
+		height: 72px;
 	}
 	.list_right span{
 		color: #666666;
 		font-size: 15px;
 		line-height: 72px;
 	}
+	.moodimg{
+		width: 40px;
+		height: 40px;
+		float: left;
+		margin-top: 16px;
+		margin-right: 30px;
+	}
+	.interaction{
+		float: left;
+		text-align: center;
+		padding-top: 13px;
+		font-size: 13px;
+		color: #aeaeae;
+	}
+	.interaction img{
+		width: 26px;
+		height: 26px;
+	}
 	.mycenterFill{
 		width: 100%;
 		height: 10px;
 		background: #eeeeee;
+	}
+	.friendCenter{
+		margin-bottom: 1px;
+	}
+	.share{
+		line-height: 36px;
+	    height: 36px;
+	    font-size: 14px;
+	    margin: 30px 15px 0 15px;
+	    color: #696969;
+	    display: block;
+	    border: 1px solid rgba(0, 0, 0, 0.2);
+	    background: #FFFFFF;
+	    border-radius: 5px;
+	    text-align: center;
 	}
 	
 </style>
