6552365a32710dd0c4eb0abfa234971a97fbeeb8
朱重熊 <308083469@qq.com>
2017-06-02 17:56:54 +0800

    朋友心情列表

--- public/src/component/friendsCount.vue
+++ public/src/component/friendsCount.vue
@@ -1,55 +1,64 @@
 <template id="friendsCount">
     <div >
         <div class="weui-cells friendsCount_box">
-            <span class="friendsCount_cell">A</span>
-            <a href="" class="weui-cell weui-cell_access">
+            <div class="friendsCount_cell">特别关系</div>
+            <a :href="user.friendLink" class="weui-cell weui-cell_access" v-for="user in  friend_s">
                 <div class="weui-cell__hd">
-                    <img class="img_frinedsCount" src="../images/13.jpg" alt="">
+                    <img class="img_frinedsCount" :src="user.faceUrl" alt="">
                 </div>
-                <div class="weui-cell__bd name_friendsCount">阿龙</div>
+                <div class="weui-cell__bd name_friendsCount">
+                    <template v-if="user.memoName&&user.memoName!==''">{{user.memoName}}</template>
+                    <template v-else-if="user.nickName!==''">{{user.nickName}}</template>
+                </div>
                 <div class="weui-cell__ft"></div>
             </a>
-            <a href="" class="weui-cell weui-cell_access">
-                <div class="weui-cell__hd">
-                    <img class="img_frinedsCount" src="../images/13.jpg" alt="">
-                </div>
-                <div class="weui-cell__bd name_friendsCount">阿龙</div>
-                <div class="weui-cell__ft"></div>
-            </a>
+
 
         </div>
         <div class="weui-cells friendsCount_box">
-            <span class="friendsCount_cell">A</span>
-            <a href="" class="weui-cell weui-cell_access">
+            <div v-for="(user,index) in friend_g"  :key="index">
+             <div class="friendsCount_cell" v-if="index==0">{{friend_g[index].firstCn[0]}}</div>
+                <div class="friendsCount_cell" v-if="index>0&& friend_g[index-1].firstCn[0]!=friend_g[index].firstCn[0]">{{friend_g[index].firstCn[0]}}</div>
+            <a :href="user.friendLink" class="weui-cell weui-cell_access">
                 <div class="weui-cell__hd">
-                    <img class="img_frinedsCount" src="../images/13.jpg" alt="">
+                    <img class="img_frinedsCount":src="user.faceUrl"  alt="">
                 </div>
-                <div class="weui-cell__bd name_friendsCount">阿龙</div>
+                <div class="weui-cell__bd name_friendsCount">
+                    <template v-if="user.memoName&&user.memoName!==''">{{user.memoName}}</template>
+                    <template v-else-if="user.nickName!==''">{{user.nickName}}</template>
+                </div>
                 <div class="weui-cell__ft"></div>
             </a>
-            <a href="" class="weui-cell weui-cell_access">
-                <div class="weui-cell__hd">
-                    <img class="img_frinedsCount" src="../images/13.jpg" alt="">
-                </div>
-                <div class="weui-cell__bd name_friendsCount">阿龙</div>
-                <div class="weui-cell__ft"></div>
-            </a>
+
+
+            </div>
+
 
         </div>
 
+        <div class="friendCount">{{friendCount}}位好友</div>
+        <div class="bgw">
+            <div class=" m-30 btn-gray"><a href="http://mp.weixin.qq.com/s/JMW1ZjzUNmP4ZIaH9ot6jw">朋友太少，怎样邀请好友？</a></div>
+        </div>
+        <div class="bottom-card">
+            <div class="plr15 m-30"><div class="weui-btn weui-btn_primary" id="mack_card_all">生成邀请卡</div></div>
+        </div>
+        <div class="friendCount"></div>
     </div>
 </template>
 <style>
     .friendsCount_box{
-        margin-top: 25px;
+        margin-top: 0px;
         overflow: inherit;
     }
+    .friendsCount_box div .weui-cell:first-child:before{ display: block}
+
     .friendsCount_cell{
+        line-height: 30px; padding-left: 15px;
         color: #666666;
         font-size: 14px;
-        position: absolute;
-        top:-22px;
-        left:15px;
+        background: #f5f5f5;
+
     }
     .img_frinedsCount{
         width:40px;
@@ -62,18 +71,40 @@
         font-size: 14px;
         margin-left:13px;
     }
+    .friendCount{
+        height: 56px;
+        line-height: 56px!important;
+        text-align: center;
+        color:#898989;
+        font-size: 16px;
+        background: #fff;
+    }
+
+    .bgw{ background: #fff;       padding-bottom: 25px;
+        padding-top: 5px;}
+    .btn-gray{ background: #ebeceb; line-height: 30px; text-align: center; margin-left: 15px !important; margin-right: 15px !important;
+        border-radius: 5px;    border-color: #ebeceb; overflow: hidden}
+    .btn-gray a{-webkit-tap-highlight-color: rgba(0,0,0,.2) !important;  padding: 0 !important; display: inherit;; background: none;font-size: 14px;  color:#666}
+
+    .bottom-card{background: #ffffff;padding:7px 0 59px 0 ; padding-left: 15px; padding-right: 15px}
+    .bottom-card  .weui-btn{ -webkit-tap-highlight-color: rgba(0,0,0,.2) !important;}
 </style>
+
+
 <script type="text/javascript">
-    import weui from "../js/weui"
+    import weui from "../js/weui";
     var friendsCount={
         template:'#friendsCount'
     }
+
+
 
     export default {
         data() {
             return {
                 friend_s:null,
-                friend_g:null
+                friend_g:null,
+                friendCount:0
             }
         },
         mounted: function () {
@@ -87,6 +118,17 @@
                 if (data.data.data !== null) {
                     _this.friend_g=eval(data.data.data.generalFriends);
                     _this.friend_s=eval(data.data.data.specialFriends);
+                    for(let i =0;i<_this.friend_s.length;i++){
+                        _this.friend_s[i].firstCn= makePy(_this.friend_s[i].nickName);
+                        _this.friend_s[i].friendLink= web.BASE_PATH + "#/me/friendsCount/friendSet/?friendId="+_this.friend_s[i].id;
+                    }
+                    for(let i =0;i<_this.friend_g.length;i++){
+                        _this.friend_g[i].firstCn= makePy(_this.friend_g[i].nickName);
+                        _this.friend_g[i].friendLink= web.BASE_PATH + "#/me/friendsCount/friendSet/?friendId="+_this.friend_g[i].id;
+                    }
+
+                    _this.friendCount=_this.friend_g.length+_this.friend_s.length;
+
                     console.log(_this.friend_g);
                     console.log(_this.friend_s);
                 }
@@ -111,7 +153,11 @@
             }
         }
     }
-    //module.exports=friendsCount
+
+
+
+
+
 </script>
 
 
