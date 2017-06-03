5763da830e43c53813b9e2f0af46146f248f48a6
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-06-03 16:54:06 +0800

    nocare

--- public/src/component/careMe.vue
+++ public/src/component/careMe.vue
@@ -1,6 +1,6 @@
 <template id="careMe">
 	<div class="careMe_box">
-		<div class="careMe_list">
+		<!--<div class="careMe_list">
 			<img class="careMe_img" src="../images/13.jpg" alt="">
 			<div class="careMe_div">
 				<div>葫芦娃</div>
@@ -10,15 +10,25 @@
 			<div class="careMe_content">
 				<img src="../images/list_mood_04.png" alt="">
 			</div>
-		</div>
-
+		</div>-->
+        <div class="noCare_box">
+            <img src="../images/nocare_pic_bj.png" alt="">
+            <div class="noCare_content">
+                <h3>还没有关心我的好友</h3>
+                <p>赶紧去生成邀请卡，分享好友互为关注</p>
+                <p>邀更多好友来一起参与记录和互为关心</p>
+            </div>
+            <div class="noCare_btn">
+                <button class="weui-btn weui-btn_primary">生成邀请卡</button>
+            </div>
+        </div>
 	</div>
 </template>
 <style>
 	.careMe_box{
-	height:100%;
+	    height:100%;
 		background: #ffffff;
-}
+    }
 	.careMe_list{
 		height:66px;
 		border-bottom: 1px solid #f5f5f5;
@@ -67,6 +77,46 @@
 		margin-top:-16px;
 		margin-left:-16px;
 	}
+    .noCare_box{
+        position: relative;
+        height:100%;
+    }
+    .noCare_box img{
+        width:180px;
+        position: absolute;
+        top:15%;
+        left:50%;
+        margin-left: -90px;
+    }
+    .noCare_content{
+        width: 100%;
+        position: absolute;
+        top:35%;
+        z-index: 1;
+        left:50%;
+        margin-left: -50%;
+    }
+    .noCare_content h3{
+        font-size: 16px;
+        color: #66cc33;
+        font-weight: 100;
+        text-align: center;
+        margin-bottom: 10px;
+    }
+    .noCare_content p{
+        font-size: 12px;
+        color: #999999;
+        text-align: center;
+        margin-bottom: 10px;
+    }
+    .noCare_btn{
+        width:90%;
+        position: absolute;
+        bottom:20%;
+        left:50%;
+        margin-left:-45%;
+    }
+
 </style>
 <script type="text/javascript">
 	var careMe={
