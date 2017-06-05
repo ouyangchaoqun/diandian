aae77bfab495ea885698a65d5e0212f829b39e79
zhangyongyong <zyy123456>
2017-06-05 18:31:40 +0800

    debug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -42,7 +42,8 @@ class Controller extends BaseController
     protected function setUserInfo(Response $response, $user)
     {
         var_dump($user);
-        $response->headers->setCookie(new Cookie("openId",$user['openId']));
+        var_dump($user['openId']);
+        $response->withCookie(new Cookie("openId",$user['openId']));
         $userId = $user['id'];
         $_SESSION['userId'] = $userId;
     }
