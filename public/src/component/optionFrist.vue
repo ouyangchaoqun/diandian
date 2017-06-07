0d4b0731b407154316d4c25694d3a93bee73aa5f
zhangyongyong <zyy123456>
2017-06-07 15:06:08 +0800

    dev

--- public/src/component/optionFrist.vue
+++ public/src/component/optionFrist.vue
@@ -37,7 +37,9 @@
                     aliossgeturl:web.BASE_PATH+'aliyunapi/oss_getsetting'
                 },
                 alioss: null,
-                pictures:[]
+                pictures:[
+
+                ]
             }
         },
         methods:{
@@ -64,73 +66,6 @@
                     }
                 }
             },
-
-            //删掉
-            _picUpRemoveLoading:function (isshow,containersize,content,id) {
-                var classname = 'up-loading';
-                id = 'up_loading_'+id;
-
-
-
-                if(isshow){
-                    var loadingpics = $('.imglist li.up-loading').length;
-                    var loadedpics = $('.imglist input[name=moodpicture]').length;
-                    var currentpics = loadingpics + loadedpics;
-                    //style="height:'+containersize.h+'px;width:'+containersize.w+'px;"
-                    var loadsize = 50;
-                    var loadingdiv = '<li class="'+classname+'" id="'+id+'"><div class="weui-loading" style="height:'+loadsize+'px;width:'
-                        +loadsize+'px;margin-top:'+(containersize.h-loadsize)/2+'px;margin-left:'+(containersize.w-loadsize)/2+'px"></div></li>';
-                    $('.imglist li.upfile').before(loadingdiv);
-                    currentpics++;
-                    if (currentpics < maxfilecount) {
-                    } else {
-                        $('.imglist li.upfile').hide();
-                    }
-                }else{
-                    if(typeof content !='undefined'){
-                        $('.imglist li#'+id).removeClass(classname).html(content);
-                        $('.imglist li#'+id).find('.del-img').unbind('click').bind('click',function () {
-                            $.xqzs.mood.removeTempPicture($(this).parent().parent(),uploadpicinfo);
-                            //
-                            var count =  getallowuploadcount();
-                            if(count>0) {
-                                $('.imglist li.upfile').show();
-                            }
-                        })
-                    }else{
-                        $('.imglist li#'+id).remove();
-                    }
-                }
-            },
-            format_show_upload_pic:function(containersize,data) {
-                var imgstyle = '';
-                if (data.width > data.height) {
-                    $w = data.width * containersize.h / data.height;
-                    imgstyle = 'height:' + containersize.h + 'px;margin-left:' + ((containersize.w - $w) / 2) + 'px;width:auto';
-                } else if (data.width < data.height) {
-                    $h = data.height * containersize.w / data.width;
-                    imgstyle = 'width:' + containersize.w + 'px;margin-top:' + ((containersize.h - $h) / 2) + 'px;height:auto';
-                } else {
-                    imgstyle = 'width:' + containersize.w + 'px;height:' + containersize.h + 'px';
-                }
-                var size = (data.width > data.height) ? ' height="100%" ' : ' width="100%" ';
-
-                var appendhtml = '<div style="height:' + containersize.h + 'px" class="img-priview"><div class="del-img"></div><img class="upload-view" style="'
-                    + imgstyle + '" src="' + data.path + uploadpicinfo.smallpic + '" data-bigsrc="' + data.path + uploadpicinfo.middlepic
-                    + '" /><input type="hidden" name="moodpicture" value="' + data.id + '" /></div>';
-                return appendhtml;
-            },
-            bind_image_slides_priview:function () {
-                $('.imglist img').unbind('click').bind('click',function () {
-                    var current = $(this).data('bigsrc');
-                    var $data = [];
-                    $('img',$('.imglist')).each(function () {
-                        $data.push($(this).data('bigsrc'));
-                    });
-                    xqzs.wx.previewImage(current,$data);
-                    // weui.galleryslide.init($data,{autoSwipe:false,index:index});
-                });
-            },
             uploadImage:function (sourceType) {
                 let that = this;
                 var id = 'uf_'+new Date().getTime();
@@ -139,18 +74,12 @@
                     h: $('.imglist').data('height')
                 };
                 //
-                console.info(that.uploadpicinfo);
-                console.info(that.alioss);
                 xqzs.wx.takePhotos(sourceType,that.maxPhotoCount,that.uploadpicinfo,that.alioss,function (filecount) {
                     for(var i=0;i<filecount;i++){
                         that._showloadingpic(containersize,id+i);
                     }
                 },function (json,ix) {
-                    var appendhtml = that.format_show_upload_pic(containersize,json.data);
-                    that._fillloadingpic(id+ix,appendhtml);
-                    //
-                    that.bind_image_slides_priview();
-
+                    that._fillloadingpic(id+ix,json.data);
                 },function (e) {
                     console.info(e);
                 })
