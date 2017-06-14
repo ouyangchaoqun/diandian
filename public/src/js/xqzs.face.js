5a0e6d65a0b6ba5c0f6415c704907e936002ab6d
zhangyongyong <zyy123456>
2017-06-14 15:36:22 +0800

    htmlencode

--- public/src/js/xqzs.face.js
+++ public/src/js/xqzs.face.js
@@ -107,10 +107,9 @@ xqzs.face = {
         s = str.replace(/&/g, "&gt;");
         s = s.replace(/</g, "&lt;");
         s = s.replace(/>/g, "&gt;");
-        s = s.replace(/ /g, "&nbsp;");
-        s = s.replace(/\'/g, "&#39;");
-        s = s.replace(/\"/g, "&quot;");
-        s = s.replace(/\n/g, "<br>");
+        //s = s.replace(/ /g, "&nbsp;");
+        //s = s.replace(/\'/g, "&#39;");
+        //s = s.replace(/\"/g, "&quot;");
         return s;
     },
     parse: function (content) {
