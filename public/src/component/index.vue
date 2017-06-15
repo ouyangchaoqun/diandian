f3ac14795e0a8cec7c3c8e6c35dd027a5516b09c
朱重熊 <308083469@qq.com>
2017-06-15 14:34:50 +0800

    首页样式修改

--- public/src/component/index.vue
+++ public/src/component/index.vue
@@ -201,7 +201,7 @@
                 this.recordImg= web.IMG_PATH+"face2.png";
                 this.recordOn=true;
                 setTimeout(function () {
-                   // _this.$router.push(_this.linkTo)
+                    _this.$router.push(_this.linkTo)
                 },2)
             },
             calendar:function () {
@@ -485,12 +485,12 @@
 
     #tabs a span{ display: inline-block; height: 24px ; width: 24px;}
     #tabs a span.recordImg{ background: url(../images/face1.png) no-repeat; background-size: 24px;}
-    #tabs a span.calendarImg{ background: url(../images/rl1.png) no-repeat; background-size: 24px; }
+    #tabs a span.calendarImg{ background: url(../images/rl1.png) no-repeat; background-size: 24px 24px; }
     #tabs a span.friendsImg{ background: url(../images/friend1.png) no-repeat; background-size: 24px;}
     #tabs a span.meImg{ background: url(../images/me1.png) no-repeat; background-size: 24px; }
 
     #tabs a:active span.recordImg{ background: url(../images/face2.png) no-repeat; background-size: 24px;}
-    #tabs a:active span.calendarImg{ background: url(../images/rl2.png) no-repeat; background-size: 24px; }
+    #tabs a:active span.calendarImg{ background: url(../images/rl2.png) no-repeat; background-size: 24px 24px; }
     #tabs a:active span.friendsImg{ background: url(../images/friend2.png) no-repeat; background-size: 24px;}
     #tabs a:active span.meImg{ background: url(../images/me2.png) no-repeat; background-size: 24px; }
 
