126e9678ff4c395be505acd5d76d1a5db61ca0b9
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-06-19 14:34:48 +0800

    moodDetail

---
+++ public/src/routes/moodDetail.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pc on 2017/6/5.
+ */
+const moodDetail = resolve => require(['../component/moodDetail.vue'], resolve)
+module.exports = {
+    path: '/moodDetail',
+    component: moodDetail,
+
+}
