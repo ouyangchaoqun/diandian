78e8f6488216c6d3555337c44971078e6636908c
Ö캺ͦ <409199338@qq.com>
2017-06-27 13:28:16 +0800

    关注我的中的生成邀请卡

--- public/src/component/careMe.vue
+++ public/src/component/careMe.vue
@@ -29,7 +29,7 @@
 				<p>邀更多好友来一起参与记录和互为关心</p>
 			</div>
 			<div class="noCare_btn">
-				<button class="weui-btn weui-btn_primary">生成邀请卡</button>
+				<button class="weui-btn weui-btn_primary" @click="createinvite()">生成邀请卡</button>
 			</div>
 		</div>
 	</div>
@@ -179,6 +179,7 @@
             }
         },
 		methods:{
+
             formatContent: function (item) {
 
                 return   xqzs.face.parse(item);
@@ -223,7 +224,37 @@
 				}, function (error) {
 					//error
 				});
-			}
+			},
+            _createinvite:function (type,callback) {
+                this.$http({
+                    method: 'GET',
+                    type: "json",
+                    url: web.API_PATH + 'wei/xin/create/invite/' + type + '/_userId_',
+                }).then(function (data) {
+                    if (data && data.data.status == 1) {
+                        if (typeof callback == 'function') {
+                            callback();
+                        }
+                    }
+                })
+            },
+            createinvite:function () {
+                let that = this;
+                that._createinvite('link',function () {
+                    that._createinvite('card',function () {});
+                    xqzs.weui.dialog({
+                        title:'邀请卡已经发送',
+                        msg:'前往公众号查看，分享好友互为关注',
+                        submitText:'查看',
+                        submitFun:function () {
+                            try {
+                                WeixinJSBridge.call('closeWindow');
+                            }catch (e){
+                            }
+                        }
+                    })
+                });
+            },
 		}
 	}
 </script>
