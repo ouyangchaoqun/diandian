64b430a6996c261a6986afe5ebf3d9fb4261292b
朱重熊 <308083469@qq.com>
2017-07-06 08:41:32 +0800

    选择场景 文字修改

--- public/src/component/addMood.vue
+++ public/src/component/addMood.vue
@@ -22,7 +22,7 @@
                 </div>
                 <div class="scene_box">
                     <div>
-                        <div class="moodBox_header">心情场景</div>
+                        <div class="moodBox_header">选择场景</div>
                         <div class="weui-grids grids_box">
                             <a v-for="scenes in scenesList" class="weui-grid grid_33" @click="chooseData('scenesId',scenes.value)" v-if="!scenes.hide">
                                 <div class="scene_top" v-if="scenes.haspic">
