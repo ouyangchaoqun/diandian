8d89353ff881a7d13d78a177f9cfec503afca8d6
朱重熊 <308083469@qq.com>
2017-07-12 17:30:26 +0800

    6月14日前的统计不计入

--- public/src/component/moodData.vue
+++ public/src/component/moodData.vue
@@ -89,7 +89,7 @@
     }
 </script>
 <style>
-    .other{font-size: 14px; color:#666; line-height: 50px;}
+    .dataLists .other{font-size: 14px; color:#666; line-height: 50px; text-align: center}
     .moodDataBox{
         background: #fff;
     }
