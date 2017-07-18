23b62a7bb6f7cc01bc1badf57e88d48ceb232f94
朱重熊 <308083469@qq.com>
2017-07-18 17:21:35 +0800

    test map in

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -76,7 +76,7 @@ if (process.env.NODE_ENV === 'production') {
             }
         }),
         new webpack.optimize.UglifyJsPlugin({
-            sourceMap: false,
+            sourceMap: true,
             compress: {
                 warnings: false
             }
