0a1b64b50b3a8bd199d46a4b9a53a5d6a727a5c7
Ö캺ͦ <409199338@qq.com>
2017-07-24 14:35:44 +0800

    record 图片加载 以及调整

--- public/src/App.vue
+++ public/src/App.vue
@@ -283,13 +283,11 @@
 
     @keyframes fold-right-out {
         0% {
-            opacity: 0;
             transform: translate3d(0%, 0, 0);
             -webkit-transform: translate3d(0%, 0, 0);
         }
 
         100% {
-            opacity: 1;
             transform: translate3d(100%, 0, 0);
             -webkit-transform: translate3d(100%, 0, 0);
         }
