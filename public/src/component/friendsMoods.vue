057f2bbb3164d909515a0680b8ab4bd4ee04156f
朱重熊 <308083469@qq.com>
2017-07-26 09:38:53 +0800

    打卡小树洞改为随即头像

--- public/src/component/friendsMoods.vue
+++ public/src/component/friendsMoods.vue
@@ -13,8 +13,7 @@
                     <router-link to="/treeHole">你知道“小树洞”的故事吗？</router-link>
                 </div>
                 <div class="friends_mood" v-for="( item,index)  in downdata" :key="index">
-                    <img class="friendHeaderImg" :src="item.randomFaceUrl" alt="" v-show="item.type==1">
-                    <img class="friendHeaderImg" :src="item.typeFaceUrl" alt="" v-show="item.type!=1">
+                    <img class="friendHeaderImg" :src="item.randomFaceUrl" alt="">
                     <div class="friendState">
                         <span class="mood_state" :class="item.moodValueStyle"><span :class="{bluecolor:item.isAd==1}" :style="item.typeFaceColor">{{item.moodValueText}}</span></span>
                         <!--<img class="addCj" :src="item.scense.src" alt="">  <i>{{item.scense.text}}</i>-->
