8656bb46ff1c1bb671bb0efe11881698b089ad1b
朱重熊 <308083469@qq.com>
2017-08-22 17:23:47 +0800

    分享图片

--- public/src/component/birthday.vue
+++ public/src/component/birthday.vue
@@ -941,7 +941,7 @@
             xqzs.wx.setConfig(this,function () {
                 wx.showAllNonBaseMenuItem();
                 var config ={
-                    imgUrl:web.IMG_PATH+"/happy_top.png",
+                    imgUrl:web.BASE_PATH+web.IMG_PATH+"/happy_top.png",
                     title: '我生日了给我祝福把',
                     desc: '我的生日到了',
                     link: web.BASE_PATH+"/guest/#/birthday?userId="+_this.birthdayUserId,
