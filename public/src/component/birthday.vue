e47aaa17bda1dc2926f339e56e4f48b0f3cb0231
朱重熊 <308083469@qq.com>
2017-08-22 09:11:37 +0800

    生日详情分享2

--- public/src/component/birthday.vue
+++ public/src/component/birthday.vue
@@ -835,7 +835,7 @@
                     imgUrl:web.IMG_PATH+"/happy_top.png",
                     title: '我生日了给我祝福把',
                     desc: '我的生日到了',
-                    link: encodeURI(window.location.href),
+                    link: window.location.href,
                 };
                 weshare.init(wx,config)
             });
