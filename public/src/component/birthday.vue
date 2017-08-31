ce0bec03417dc11772963464d8453d24e7eabc43
朱重熊 <308083469@qq.com>
2017-08-31 10:35:46 +0800

    图片修改

--- public/src/component/birthday.vue
+++ public/src/component/birthday.vue
@@ -1108,7 +1108,7 @@
                         xqzs.wx.setConfig(this, function () {
                             wx.showAllNonBaseMenuItem();
                             var config = {
-                                imgUrl: web.BASE_PATH + web.IMG_PATH + "/birthday/birthday_share.jpg",
+                                imgUrl: web.BASE_PATH + web.IMG_PATH + "/birthday/birthday_share2.jpg",
                                 title: '生日祝福',
                                 desc: "今天是“"+_this.birthdayUser.nickName + '”生日，快来送个祝福吧！',
                                 link: web.BASE_PATH + "/guest/#/birthday?userId=" + _this.birthdayUserId,
