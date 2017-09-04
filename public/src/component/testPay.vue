7ee0739cd797360ecf0a2c2805ae8aae2ed65e66
朱重熊 <308083469@qq.com>
2017-09-04 17:42:31 +0800

    pay test

--- public/src/component/testPay.vue
+++ public/src/component/testPay.vue
@@ -1,6 +1,9 @@
 <template id="testPay">
     <div class="testPay_box">
-        <div class="btn" style="background:red; text-align: center; color:#fff; height: 55px; line-height: 55px; margin-top: 60px;" @click="pay">支付</div>
+        <div class="btn"
+             style="background:red; text-align: center; color:#fff; height: 55px; line-height: 55px; margin-top: 60px;"
+             @click="pay">支付
+        </div>
 
     </div>
 </template>
@@ -8,53 +11,54 @@
 
 </style>
 <script type="es6">
-     let testPay = {
+    let testPay = {
         template: '#testPay'
     };
 
     export default {
 
         data() {
-            return {
-                
-            }
+            return {}
         },
         methods: {
-            pay:function () {
-                let _this =this;
+            onBridgeReady:function(config) {
+                WeixinJSBridge.invoke(
+                    'getBrandWCPayRequest', config,
+                    function (res) {
+                        // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                        if (res.err_msg == "get_brand_wcpay_request:ok") {
+
+
+                        }
+                    }
+                );
+            },
+
+            pay: function () {
+                let _this = this;
                 _this.$http.put(web.API_PATH + 'power/plan/_userId_/' + 1 + '/' + 1 + '').then(function (res) {
 
-                    function onBridgeReady() {
-                        WeixinJSBridge.invoke(
-                            'getBrandWCPayRequest', {
-                                "appId":res.data.appid,     //公众号名称，由商户传入
-                                "timeStamp":res.data.time,        //时间戳，自1970年以来的秒数
-                                "nonceStr":res.data.nonce_str , //随机串
-                                "package":"prepay_id="+res.data.prepay_id+"",
-                                "signType":"MD5",         //微信签名方式：
-                                "paySign":res.data.sign //微信签名
-                            },
-                            function(res){
-                                // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-                                if(res.err_msg == "get_brand_wcpay_request:ok" ) {
 
-
-
-
-                                }
-                            }
-                        );
+                    let config ={
+                        "appId": res.data.appid,     //公众号名称，由商户传入
+                        "timeStamp": res.data.time,        //时间戳，自1970年以来的秒数
+                        "nonceStr": res.data.nonce_str, //随机串
+                        "package": "prepay_id=" + res.data.prepay_id + "",
+                        "signType": "MD5",         //微信签名方式：
+                        "paySign": res.data.sign //微信签名
                     };
 
-                    if (typeof WeixinJSBridge == "undefined"){
-                        if( document.addEventListener ){
-                            document.addEventListener('WeixinJSBridgeReady', _this.onBridgeReady, false);
-                        }else if (document.attachEvent){
-                            document.attachEvent('WeixinJSBridgeReady',  _this.onBridgeReady);
-                            document.attachEvent('onWeixinJSBridgeReady',  _this.onBridgeReady);
+
+
+                    if (typeof WeixinJSBridge == "undefined") {
+                        if (document.addEventListener) {
+                            document.addEventListener('WeixinJSBridgeReady', _this.onBridgeReady(config), false);
+                        } else if (document.attachEvent) {
+                            document.attachEvent('WeixinJSBridgeReady', _this.onBridgeReady(config));
+                            document.attachEvent('onWeixinJSBridgeReady', _this.onBridgeReady(config));
                         }
-                    }else{
-                        _this.onBridgeReady();
+                    } else {
+                        _this.onBridgeReady(config);
                     }
 
 
