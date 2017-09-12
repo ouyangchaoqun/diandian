1ee31b73b9351ab77f03672cc620aad15bab80a7
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-09-12 18:17:51 +0800

    心理测试试题

--- public/src/component/testQuestions.vue
+++ public/src/component/testQuestions.vue
@@ -6,14 +6,81 @@
             </div>
         </div>
         <div class="testQuestions_process">1/10</div>
-        <div class="question_content">生活压力越来越重，都市人得忧郁症越来越普遍，你忧郁了吗？请依据一周以来身体，情绪的感觉选择与自己最相符的一项。</div>
-        <ul class="question_option">
-            <li>
-                <span class="optionItem">A</span><span>会，怕失望，怕付出和得到不成正比</span>
+        <div class="swiper-container question_box">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div>
+                        <div class="question_content">生活压力越来越重，都市人得忧郁症越来越普遍，你忧郁了吗？请依据一周以来身体，情绪的感觉选择与自己最相符的一项。</div>
+                        <ul class="question_option">
+                            <li class="nextOption">
+                                <span class="optionItem">A</span><span>会，怕失望，怕付出和得到不成正比</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
+                            <li class="nextOption">
+                                <span class="optionItem">B</span><span>不会，时间是必然要付出的</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
 
-            </li>
-            <li><span class="optionItem">B</span><span>不会，时间是必然要付出的</span></li>
-        </ul>
+                        </ul>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div>
+                        <div class="question_content">生活压力越来越重，都市人得忧郁症越来越普遍，你忧郁了吗？请依据一周以来身体，情绪的感觉选择与自己最相符的一项。</div>
+                        <ul class="question_option">
+                            <li class="nextOption">
+                                <span class="optionItem">A</span><span>会，怕失望，怕付出和得到不成正比</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
+                            <li class="nextOption">
+                                <span class="optionItem">B</span><span>不会，时间是必然要付出的</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div>
+                        <div class="question_content">生活压力越来越重，都市人得忧郁症越来越普遍，你忧郁了吗？请依据一周以来身体，情绪的感觉选择与自己最相符的一项。</div>
+                        <ul class="question_option">
+                            <li class="nextOption">
+                                <span class="optionItem">A</span><span>会，怕失望，怕付出和得到不成正比</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
+                            <li class="nextOption">
+                                <span class="optionItem">B</span><span>不会，时间是必然要付出的</span>
+                                <label class="questLabel">
+                                    <input class="questRadio" type="radio" name="questRadio">
+                                    <span class="questRadioInput"></span>
+                                </label>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="weui-btn weui-btn_primary prevOption">上一题</div>
+
     </div>
 </template>
 <script type="text/javascript">
@@ -27,7 +94,17 @@
             }
         },
         mounted: function () {
+            var questSwiper = new Swiper ('.question_box', {
+                direction: 'horizontal',
+                // 如果需要前进后退按钮
+                nextButton: '.nextOption',
+                prevButton: '.prevOption'
+            })
+            $('.nextOption').on('click',function () {
+                $(this).siblings().find('input').removeAttr('checked')
+                $(this).find('input').attr('checked','checked')
 
+            })
         },
         methods: {
 
@@ -36,6 +113,30 @@
     }
 </script>
 <style>
+    .questLabel{
+        float: right;
+        margin-right: 5px   ;
+    }
+    .questRadio{display:none}
+    .questRadioInput{background-color:#fff;
+        border:1px solid #1AAC19;
+        border-radius:50%;
+        display:inline-block;
+        height:15px;
+        margin-top:-3px;
+        vertical-align:middle;
+        width:15px;
+        line-height:1
+    }
+    .questRadio:checked + .questRadioInput:after{
+        background-color:#1AAC19;
+        border-radius:50%;content:"";
+        display:inline-block;
+        height:11px;
+        width:11px;
+        margin:2px;
+        text-align: center;
+    }
     .testQuestions{
         background: #fff;
     }
@@ -59,14 +160,22 @@
         color: #666;
         font-size: 13px;
     }
-    .question_option li{
+    .nextOption{
         padding:15px 20px;
         border-bottom: 1px solid #eee;
+    }
+    .nextOption:active{
+        background: #ECECEC;
     }
     .question_option li:last-of-type{
         border-bottom: 0;
     }
     .optionItem{
         margin-right: 15px;
+    }
+    .prevOption{
+        width:150px;
+        font-size: 16px;
+        margin-top: 35px;
     }
 </style>
