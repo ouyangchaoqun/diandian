d9af00f45f2135561e1e60439471156515d87e8e
朱重熊 <308083469@qq.com>
2017-09-14 12:03:05 +0800

    支付+逻辑

--- public/src/component/psychtestDetail.vue
+++ public/src/component/psychtestDetail.vue
@@ -12,7 +12,7 @@
         <div class="psych_test_btn_box">
         <template v-if="testDetail.answerId!=null">
                 <div class="psych_test_btn_view" @click="viewResult()">查看报告</div>
-                <div class="psychtestDetail_btn" @click="startTest()">重新测试</div>
+                <!--<div class="psychtestDetail_btn" @click="startTest()">重新测试</div>-->
         </template>
         <template v-if="testDetail.answerId==null">
             <div class="psychtestDetail_btn" @click="startTest()">立即测试</div>
