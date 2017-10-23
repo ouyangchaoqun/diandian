7af0bb26a31f6b5a8197234083df7a7c571a492c
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-10-23 09:16:37 +0800

    优化

--- public/src/component/psychtestDetail.vue
+++ public/src/component/psychtestDetail.vue
@@ -1,71 +1,73 @@
 <template id="psychtestDetail">
-    <div class="psychtestDetail">
-        <div class="psychtestDetail_header">
-            <img :src="testDetail.pic" alt="">
-            <h2 class="psychtestDetail_title">{{testDetail.title}}</h2>
-            <div class="psychtestDetail_class">{{testDetail.class_name}}</div>
-            <div class="psychtestDetail_flex">
-                <div>{{testDetail.question_count}}道精选问题</div>
-                <div>1页专业报告</div>
-                <div>{{testDetail.count}}+W人测试过</div>
-            </div>
-        </div>
-        <div class="psychtestDetail_main">
-            <div class="addBottom">
-                <div>
-                    <div class="addBottomLine">评测介绍</div>
-                    <div class="bottom_line"></div>
-                </div>
-                <div class="addBottomText" v-html="testDetail.des">
+    <div class="psychtestDetail" >
+        <v-showLoad v-if="showLoad"></v-showLoad>
+        <div v-if="htmlover">
+            <div class="psychtestDetail_header" >
+                <img :src="testDetail.pic" alt="">
+                <h2 class="psychtestDetail_title">{{testDetail.title}}</h2>
+                <div class="psychtestDetail_class">{{testDetail.class_name}}</div>
+                <div class="psychtestDetail_flex">
+                    <div>{{testDetail.question_count}}道精选问题</div>
+                    <div>1页专业报告</div>
+                    <div>{{testDetail.count}}+W人测试过</div>
                 </div>
             </div>
-        </div>
-        <div class="psychtestDetail_main">
-            <div class="addBottom">
-                <div>
-                    <div class="addBottomLine">评测须知</div>
-                    <div class="bottom_line"></div>
-                </div>
-                <div class="addBottomText" style="margin-bottom: 2.588rem">
-                    <div>① 本测试为付费测试，付费成功后将自动跳转
-                        到答题页；</div>
-                    <div> ② 为保证准确性，一次付费仅能测试一次，测
-                        试结果一旦生成，想进行二次测试需要再次付
-                        费；</div>
-                    <div> ③ 请根据实际情况作答，答题结束后会生成一
-                        份专业的测评报告，若用户进行多次测试，我们
-                        只保留最近一次的测试结果。</div>
+            <div class="psychtestDetail_main">
+                <div class="addBottom">
+                    <div>
+                        <div class="addBottomLine">评测介绍</div>
+                        <div class="bottom_line"></div>
+                    </div>
+                    <div class="addBottomText" v-html="testDetail.des">
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="psychtestDetail_main">
+                <div class="addBottom">
+                    <div>
+                        <div class="addBottomLine">评测须知</div>
+                        <div class="bottom_line"></div>
+                    </div>
+                    <div class="addBottomText" style="margin-bottom: 2.588rem">
+                        <div>① 本测试为付费测试，付费成功后将自动跳转
+                            到答题页；</div>
+                        <div> ② 为保证准确性，一次付费仅能测试一次，测
+                            试结果一旦生成，想进行二次测试需要再次付
+                            费；</div>
+                        <div> ③ 请根据实际情况作答，答题结束后会生成一
+                            份专业的测评报告，若用户进行多次测试，我们
+                            只保留最近一次的测试结果。</div>
+                    </div>
+                </div>
+            </div>
 
-        <div class="psych_test_btn_box">
-            <!--<template v-if="testDetail.lastAnswerId!=null">-->
+            <div class="psych_test_btn_box">
+                <!--<template v-if="testDetail.lastAnswerId!=null">-->
                 <!--<div class="psych_test_btn_view" @click="viewResult()">查看报告</div>-->
                 <!--<div class="psychtestDetail_btn" @click="startTest()">-->
-                    <!--<template v-if="payed==1">完成测试</template>-->
-                    <!--<template v-if="payed==0">重新测试</template>-->
+                <!--<template v-if="payed==1">完成测试</template>-->
+                <!--<template v-if="payed==0">重新测试</template>-->
 
-                    <!--</div>-->
-            <!--</template>-->
-            <!--<template v-if="testDetail.lastAnswerId==null">-->
+                <!--</div>-->
+                <!--</template>-->
+                <!--<template v-if="testDetail.lastAnswerId==null">-->
                 <!--<div class="psychtestDetail_btn" @click="startTest()">-->
-                    <!--<template v-if="payed==1">完成测试</template>-->
-                    <!--<template v-if="payed==0">立即测试</template></div>-->
-            <!--</template>-->
+                <!--<template v-if="payed==1">完成测试</template>-->
+                <!--<template v-if="payed==0">立即测试</template></div>-->
+                <!--</template>-->
 
-            <template>
-                <div class="psych_test_btn_price" :class="{noTestStyle:testDetail.lastAnswerId==null}">￥{{testDetail.price}} <span>{{testDetail.old_price}}</span></div>
-                <div class="psych_test_btn_view" v-if="testDetail.lastAnswerId!=null" @click="viewResult()">查看报告</div>
-                <div class="psychtestDetail_btn" :class="{noTestStyle:testDetail.lastAnswerId==null}" @click="startTest()">立即购买</div>
-            </template>
+                <template>
+                    <div class="psych_test_btn_price" :class="{noTestStyle:testDetail.lastAnswerId==null}">￥{{testDetail.price}} <span>{{testDetail.old_price}}</span></div>
+                    <div class="psych_test_btn_view" v-if="testDetail.lastAnswerId!=null" @click="viewResult()">查看报告</div>
+                    <div class="psychtestDetail_btn" :class="{noTestStyle:testDetail.lastAnswerId==null}" @click="startTest()">立即购买</div>
+                </template>
+            </div>
         </div>
-
-
     </div>
 </template>
 <script type="text/javascript">
     import Bus from '../component/bus.js';
+    import showLoad from './showLoad.vue';
     var psychtestDetail = {
         template: '#psychtestDetail'
     }
@@ -75,13 +77,18 @@
                 testId: '',
                 testDetail: {},
                 payed: 0,
+                showLoad: false,
+                htmlover:false
             }
         },
         mounted: function () {
             let _this = this;
+            _this.showLoad=true;
             _this.testId = _this.$route.query.testId;
             let start = _this.$route.query.start;
             _this.$http.get(web.API_PATH + 'test/get/' + _this.testId + '/_userId_').then(response => {
+                _this.showLoad=false;
+                _this.htmlover = true
                 if (response.data.status === 1) {
                     console.log(response.data.data);
                     _this.testDetail = response.data.data;
@@ -96,6 +103,9 @@
                 // error
 
             });
+        },
+        components: {
+            'v-showLoad': showLoad
         },
         methods: {
             startTest: function () {
