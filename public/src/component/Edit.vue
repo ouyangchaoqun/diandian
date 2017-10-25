2971d7c460298c4197919fa58e576bf65e9a24b5
朱重熊 <308083469@qq.com>
2017-10-25 12:16:42 +0800

    公开不公开

--- public/src/component/Edit.vue
+++ public/src/component/Edit.vue
@@ -36,8 +36,10 @@
                     <div><img class="optionThirdNo" src="../images/cantfunny.png" alt=""></div>
                 </div>
                 <div class="open_box">
+                    <div class="wwww">
                     <span class="open_btn" @click.stop="changeOpen(1)" :class="{on:isopen==1}">公开</span>
                     <span class="open_btn"  @click.stop="changeOpen(0)"  :class="{on:isopen!=1}">不公开</span>
+                    </div>
                 </div>
                 <!--<div><div class="optionFourth" :class="openstyle" @click="changeisopen()">{{isopen==1?'匿名公开':'不公开'}}</div></div>-->
             </div>
@@ -708,14 +710,15 @@
     }
 </script>
 <style>
-    .edit_option  .open_box{ border-left:1px solid #eee; height: 1.4rem !important; font-size: 0.7rem; color:#999; padding-left: 0.8rem;
-        -webkit-box-flex:2;
-        -webkit-flex: 2;
-        flex: 2;}
-    .edit_option  .open_box .open_btn{float:left;display: inline-block; margin-right: 0.6rem;}
-    .edit_option  .open_box .open_btn:last-child{ margin-right: 0}
+    .edit_option  .open_box{ border-left:1px solid #eee; height: 1.4rem !important; font-size: 0.7rem; color:#999;
+        -webkit-box-flex:1.68;
+        -webkit-flex: 1.68;
+        flex: 1.68; overflow: auto }
+    .edit_option  .open_box .open_btn{float:left;display: inline-block; margin-left: 0.3rem;}
     .edit_option  .open_box .open_btn:before{content: ' '; border: 1px solid #999; margin-right: 0.2rem; margin-top: 0.2rem; display: block; height: 0.6rem; width: 0.6rem; border-radius: 50%; float:left;}
     .edit_option  .open_box .open_btn.on:before{ border: 1px solid #0b900c; background: #0BB20C}
+    .edit_option  .open_box  .wwww{ width: 108px}
+
     .addEdit{
         height:65px;
         margin-bottom:15px;
