064b7fdc068a4771cafeccf64b80bcd6572106bf
朱重熊 <308083469@qq.com>
2017-11-03 17:34:08 +0800

    分享

--- public/src/component/psychtestDetail.vue
+++ public/src/component/psychtestDetail.vue
@@ -152,15 +152,15 @@
                         }
                     }
                     xqzs.wx.setConfig(this, function () {
-//                    wx.showAllNonBaseMenuItem();
-//                    var config = {
-//
-//                        imgUrl:_this.testDetail.share_pic,
-//                        title: _this.testDetail.share_title,
-//                        desc: _this.testDetail.share_description,
-//                        link: web.BASE_PATH + "wx/pub/#/psychtestDetail?testId=" + _this.testId,
-//                    };
-//                    weshare.init(wx,config)
+                    wx.showAllNonBaseMenuItem();
+                    var config = {
+
+                        imgUrl:_this.testDetail.share_pic,
+                        title: _this.testDetail.share_title,
+                        desc: _this.testDetail.share_description,
+                        link: web.BASE_PATH + "wx/pub/#/psychtestDetail?testId=" + _this.testId,
+                    };
+                    weshare.init(wx,config)
                     });
 
                 }, response => {
