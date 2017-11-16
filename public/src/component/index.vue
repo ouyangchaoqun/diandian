69619cfcc7c6c043c69204429e8dec4eebe9667f
朱重熊 <308083469@qq.com>
2017-11-16 15:20:07 +0800

    首页调整

--- public/src/component/index.vue
+++ public/src/component/index.vue
@@ -628,10 +628,12 @@
 
     @keyframes  flow {
         0%{
-            left:0
+            transform: translate3d(0, 0, 0);
+            -webkit-transform: translate3d(0, 0, 0);
         }
         100%{
-            left:-100%;
+            transform: translate3d(-100%, 0, 0);
+            -webkit-transform: translate3d(-100%, 0, 0);
         }
     }
 
