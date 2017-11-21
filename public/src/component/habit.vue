0872bd5b3f74782352994d61a72c565d1dbf534c
朱重熊 <308083469@qq.com>
2017-11-21 11:35:14 +0800

    habit

--- public/src/component/habit.vue
+++ public/src/component/habit.vue
@@ -1,33 +1,39 @@
 <template >
     <div class="habit_box">
         <v-showLoad v-if="showLoad"></v-showLoad>
-        <div class="my_habit">
 
-            <div class="item " v-for="(item,index) in habits " @click="delOrAddHabit(index)" :class="{on:item.todayAdded==1}" >
-                <div class="img"  v-if="item.todayAdded==1" :style="'background: url('+item.iconFinish+') no-repeat center; background-size: 40%;'"></div>
-                <div class="img" v-else="" :style="'background: url('+item.iconNotFinish+') no-repeat center; background-size: 40%;'"></div>
-                <div class="txt">{{item.title}}</div>
-            </div>
+        <v-scroll :on-refresh="onRefresh" :on-infinite="onInfinite"  :isPageEnd="isPageEnd" :isShowMoreText="isShowMoreText">
+            <div class="my_habit">
 
-            <div class="item" @click="goAdd()">
-                <div class="img">+</div>
-                <div class="txt">编辑</div>
-            </div>
-            <div class="clear"></div>
+                <div class="item " v-for="(item,index) in habits " @click="delOrAddHabit(index)" :class="{on:item.todayAdded==1}" >
+                    <div class="img"  v-if="item.todayAdded==1" :style="'background: url('+item.iconFinish+') no-repeat center; background-size: 40%;'"></div>
+                    <div class="img" v-else="" :style="'background: url('+item.iconNotFinish+') no-repeat center; background-size: 40%;'"></div>
+                    <div class="txt">{{item.title}}</div>
+                </div>
 
-        </div>
-        <div class="habit_history">
-            <div class="title">历史习惯</div>
-            <div class="item" v-for="item in list">
-                <div class="time">{{item.month}}.{{item.day}}</div>
-                <div class="day_habits">
-                    <span v-for="i in item.habits"><img :src="i.iconFinish" /></span>
-
+                <div class="item" @click="goAdd()">
+                    <div class="img">+</div>
+                    <div class="txt">编辑</div>
                 </div>
                 <div class="clear"></div>
+
+            </div>
+            <div class="habit_history">
+                <div class="title">历史习惯</div>
+                <div class="item" v-for="item in list">
+                    <div class="time" v-if="item.day<10">{{item.month}}.0{{item.day}}</div>
+                    <div class="time" v-else="">{{item.month}}.{{item.day}}</div>
+                    <div class="day_habits">
+                        <span v-for="i in item.habits"><img :src="i.iconFinish" /></span>
+
+                    </div>
+                    <div class="clear"></div>
+                </div>
+
             </div>
 
-        </div>
+
+        </v-scroll>
         <div class="habit_btns">
             <div class="btn btn_s" @click="goStatistics()">
                 <div class="img"></div>
@@ -38,6 +44,7 @@
                 <div class="txt">卡片</div>
             </div>
         </div>
+
         <div class="addHabit" v-if="animit" >
             <div class="r"></div>
             <div class="good"></div>
@@ -45,7 +52,8 @@
     </div>
 </template>
 <style>
-    .habit_box{ background: #fff}
+    .habit_box ,    .habit_box .yo-scroll{ background: #fff}
+
     .habit_box .my_habit{ padding: 0.6rem 0.88235rem; padding-bottom: 1rem; border-bottom: #F5F5F5 solid 0.55882352941176470588235294117647rem}
     .habit_box .my_habit .item{  width: 25%; float:left; text-align: center; color:#999; font-size: 0.70588235294117647058823529411765rem ;  margin-top: 0.8rem;  }
     .habit_box .my_habit .item .img{  display: inline-block;border: 1px solid rgba(217,217,217,0.5) ; height: 2.9411764705882352941176470588235rem; width: 2.9411764705882352941176470588235rem;  border-radius: 50%; font-size: 2rem;  line-height:2.7rem;  overflow: hidden;}
@@ -58,9 +66,9 @@
     .habit_box .habit_history .title{ color:#4A4949; font-size:0.82352941176470588235294117647059rem; font-weight: bold; padding-left: 0.88235rem; padding-top: 0.6rem; }
     .habit_box .habit_history .item .time{ font-size: 0.70588235294117647058823529411765rem;color:#999;float:left;line-height:2rem; margin-left: 0.88235rem }
     .habit_box .habit_history:before{width: 1px; height:100%; background: #eee; content: ' '; display: block; position: absolute; top:2rem; left:3.4rem;  }
-    .habit_box .habit_history .item .day_habits img{ height:1rem; }
+    .habit_box .habit_history .item .day_habits img{ height:1.3rem; }
     .habit_box .habit_history .item .day_habits{ line-height: 2.2rem; margin-left: 1.6rem;float:left}
-    .habit_box .habit_history .item .day_habits span{display: inline-block; width:2rem; text-align: center }
+    .habit_box .habit_history .item .day_habits span{display: inline-block; width:2.3rem; text-align: center }
     .habit_box .habit_history .item{ position: relative}
     .habit_box .habit_history .item:before{content:"";display:block; position:absolute; top:.8rem; left:3.3rem;height: 0.29411764705882352941176470588235rem;width: 0.29411764705882352941176470588235rem; border-radius: 50%; background: #FF9900}
 
@@ -159,6 +167,7 @@
 </style>
 <script type="text/javascript">
     import Bus from './bus.js';
+    import scroll from './lib/scroll.vue';
     import showLoad from './showLoad.vue';
     export default {
         data() {
@@ -166,7 +175,7 @@
                 habits:[],
                 list:[],
                 page: 1,
-                row: 12,
+                row: 14,
                 isPageEnd: false,
                 isShowMoreText:false,
                 showLoad:false,
@@ -315,6 +324,7 @@
             }
         },
         components: {
+            'v-scroll': scroll,
             'v-showLoad': showLoad
         }
     }
