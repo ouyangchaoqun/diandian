94c7b4c276cadfff7a38cdc8965e67a11478517f
朱重熊 <308083469@qq.com>
2017-12-01 17:42:36 +0800

    积分列表+地址

--- public/src/component/coin/address.vue
+++ public/src/component/coin/address.vue
@@ -1,6 +1,40 @@
 <template>
     <div class="coin_address">
 
+        <div class="box">
+            <div class="item">
+                <span>收货人</span>
+                <div class="con"><input type="text" placeholder="请输入收货人名字"> </div>
+            </div>
+            <div class="item">
+            <span>手机号码</span>
+            <div class="con"><input type="text" placeholder="收货人的电话，方便联系"> </div>
+        </div>
+            <div class="item" id="localCity" @click="areaPicker()">
+                <span>地址</span>
+                <div class="showdL area">
+                    <span id="address_ts">请选择地址</span>
+                    <span v-if="provinceName">{{provinceName}}</span>
+                    <span v-if="cityName">{{cityName}}</span>
+                    <span v-if="areaName">{{areaName}}</span>
+
+                </div>
+                <div style="clear: both"></div>
+                <div class="line"></div>
+            </div>
+            <div class="item addess">
+                <span>详细地址</span>
+                <div class="con"><textarea type="text" placeholder="请输入街道、门派等详细地址信息"></textarea> </div>
+             </div>
+            <div  @click="msgSubmit()">
+                <a class="submit_btn">提交</a>
+            </div>
+
+
+        </div>
+
+
+
     </div>
 </template>
 <script>
@@ -8,7 +42,13 @@
     export default {
         data() {
             return {
-
+                defaultCity: '[330000, 330100, 330102]',
+                provinceName: '',
+                cityName: '',
+                areaName: '',
+                provinceId: '',
+                cityId: '',
+                areaId: '',
             }
         },
         mounted:function () {
@@ -18,7 +58,39 @@
 
         },
         methods: {
+            areaPicker: function () {
+                let _this = this;
+                $.get('/src/js/city.json', function (data) {
+                    weui.picker(data, {
+                        depth: 3,
+                        defaultValue: _this.defaultCity,
+                        onChange: function onChange(result) {
 
+                        },
+                        onConfirm: function onConfirm(result) {
+                            if (result[0]) {
+                                _this.provinceId = result[0].value;
+                                _this.provinceName = result[0].label;
+                            }
+                            if (result[1]) {
+                                _this.cityId = result[1].value;
+                                _this.cityName = result[1].label;
+                            }
+                            if (result[2]) {
+                                _this.areaId = result[2].value;
+                                _this.areaName = result[2].label;
+                            } else {
+                                _this.areaId = '';
+                                _this.areaName = '';
+                            }
+                            $("#address_ts").html('')
+
+                        },
+                        id: 'cascadePicker'
+                    });
+                });
+
+            },
         }
     }
 
@@ -26,6 +98,12 @@
 </script>
 <style>
     .coin_address{ background: #Fff}
-
-
+    .coin_address .box{ margin: 0.88235rem;}
+    .coin_address .box .item{ height: 3.058823529411765rem; border-bottom: 1px solid  #eee; line-height:3.058823529411765rem; }
+    .coin_address .box .item>span{ display: inline-block; float:left; width:5.882352941176471rem;color:#333 ; font-size: 0.9411764705882353rem; font-weight: bold; }
+    .coin_address .box .item .con{ height: 100% ; color:#666}
+    .coin_address .box .item .con input{ height:2rem; border: none;outline:none; font-size: 0.9411764705882353rem;  }
+    .coin_address .box .submit_btn{ background: #E7972F; border-radius: 0.2rem; height:2.588235294117647rem;  line-height: 2.588235294117647rem; color:#fff ;text-align: center ; margin: 0 10%; display: block; margin-top: 2rem; }
+    .coin_address .box .addess{ height: auto}
+    .coin_address .box .addess textarea{ height:2rem; line-height: 1rem; font-size: 0.9411764705882353rem; width: 70%; border: none ; outline: none; margin-top: 1rem;}
 </style>
---
+++ public/src/component/coin/list.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="coin_list">
+
+        <div class="my_coin">
+            <div class="word">当前积分：<span>1100</span></div>
+        </div>
+        <div class="list">
+            <div class="item">
+                <div class="name">早起打卡</div>
+                <div class="time">2017-1-01</div>
+                <div class="coin">+69</div>
+            </div>
+            <div class="item">
+                <div class="name">早起打卡</div>
+                <div class="time">2017-1-01</div>
+                <div class="coin">+69</div>
+            </div>
+            <div class="item">
+                <div class="name">早起打卡</div>
+                <div class="time">2017-1-01</div>
+                <div class="coin">+69</div>
+            </div>
+            <div class="item">
+                <div class="name">早起打卡</div>
+                <div class="time">2017-1-01</div>
+                <div class="coin">+69</div>
+            </div>
+            <div class="item">
+                <div class="name">早起打卡</div>
+                <div class="time">2017-1-01</div>
+                <div class="coin">+69</div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+<script>
+
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted:function () {
+
+
+
+
+        },
+        methods: {
+
+        }
+    }
+
+
+</script>
+<style>
+    .coin_list{ background: #fff;}
+    .coin_list .my_coin{
+        height: 3.6rem;
+        background:-webkit-gradient(linear, 0 0, right 0, from(#f97f06), to(#ffb336));
+        line-height: 3.6rem; color:#fff; font-size: 0.9411764705882353rem; padding-left: 1rem;
+    }
+    .coin_list .my_coin span{ font-size:1.764705882352941rem; }
+    .coin_list  .list{ margin-left: 1rem;}
+    .coin_list  .list .item{ line-height: 1; border-bottom: 1px solid #eee;
+        height:3.1rem; padding-top: 0.9rem; position: relative}
+    .coin_list  .list .item .name{ font-size: 0.88235rem;; color:#333;}
+    .coin_list  .list .item .time{ color:#999; font-size: 0.7058823529411765rem  ; margin-top: 0.5rem;}
+    .coin_list  .list .item    .coin{ position: absolute;top:0; right:1.233rem; line-height:4rem; color:#E28B27; font-size: 1.411764705882353rem; }
+</style>
