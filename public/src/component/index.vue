c7500b5aacc92e2ed7153fac1df00ac052d4abb7
朱重熊 <251675335@qq.com>
2017-12-08 21:05:19 +0800

    bug

--- public/src/component/index.vue
+++ public/src/component/index.vue
@@ -200,7 +200,7 @@
             <!--friendcenter end-->
         </div>
         <!--早睡弹窗-->
-        <div class="sleep_dialog weui-mask weui-animate-fade-in" v-show="isGoSleep" @click="hideSleepDialog()">
+        <div class="sleep_dialog weui-mask weui-animate-fade-in" v-if="isGoSleep" @click="hideSleepDialog()">
             <div class="sleep_dialog_box">
                 <div class="sleep_dialog_title">睡觉打卡</div>
                 <p>又到睡觉时间了！乖，放下手机，关灯、睡觉，做个好梦。</p>
@@ -749,12 +749,14 @@
                 console.log('sleep')
                 let _this = this;
                 if (_this.isGoBed) {
-                    _this.$router.push("sleepRank?type=" + this.NIGHT_TYPE)
+                     _this.$router.push("sleepRank?type=" + this.NIGHT_TYPE)
                 }else{
+
                     if(_this.isRecordTime()){
                         //未打卡并在打卡时间段内
                         _this.isGoSleep = true
-                    }else{
+                     }else{
+
                         if(!_this.recordTimeOut()){
                             console.log('提前打')
 
