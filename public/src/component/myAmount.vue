1f66d06f4dbc2795212c8caba03b4fe04f5e6b3c
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2017-12-25 16:53:15 +0800

    提现测试页面

--- public/src/component/myAmount.vue
+++ public/src/component/myAmount.vue
@@ -7,7 +7,7 @@
             <div class="img"></div>
             <div class="my_income_txt">我的余额</div>
             <div class="money">￥{{formatPrice(user.balance)}}</div>
-            <div class="get_money" @click="goMoneyOut()">提现</div>
+            <!--<div class="get_money" @click="goMoneyOut()">提现</div>-->
         </div>
         <div class="moneyOut_box" v-if="isOut" @click="hideOut()">
             <div class="money_dialog">
