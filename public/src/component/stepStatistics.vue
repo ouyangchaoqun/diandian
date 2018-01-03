408b3938c744d64440c4e29ed892f00132c8b86c
ouyangchaoqun <ouyangchaoqun@aliyun.com>
2018-01-03 09:30:17 +0800

    步数bug测试

--- public/src/component/stepStatistics.vue
+++ public/src/component/stepStatistics.vue
@@ -175,16 +175,8 @@
                 let cur_year = date.getFullYear();
                 /**年份 */
                 let cur_month = date.getMonth() + 1;
-                console.log(cur_year)
-                console.log(this.cur_year)
-                console.log(cur_month)
-                console.log(this.cur_month)
-                if(cur_year==this.cur_year&&cur_month==this.cur_month){
-                    this.isLast = true;
-                }else{
-                    this.isLast = false;
-                }
-
+               let lastYear = date.getFullYear();
+                let lastMonth = date.getMonth() + 1;
 
                 /**月 */
                 this.cur_day=now;
@@ -204,6 +196,15 @@
                 this.cur_month = cur_month;
                 this._month = _month;
                 this.today = today;
+                console.log(lastYear)
+                console.log(lastMonth)
+                console.log(this.cur_year)
+                console.log(this.cur_month)
+                if(lastYear==this.cur_year&&lastMonth==this.cur_month){
+                    this.isLast = true;
+                }else{
+                    this.isLast = false;
+                }
 
             },
             getThisMonthDays(year, month) {
