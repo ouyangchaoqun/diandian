e6e9ea78c175be9b9370bed19c19978889f6efad
朱重熊 <308083469@qq.com>
2018-01-12 14:39:54 +0800

    bug

--- public/src/js/xqzs.js
+++ public/src/js/xqzs.js
@@ -60,7 +60,10 @@ var xqzs = {
             setTimeout(function () {
                 $("#toast").animate({opacity: 0}, 200, function () {
                     $("#toast").remove();
-                    fun();
+                    if(typeof  fun =='function'){
+                        fun();
+                    }
+
                 });
             }, 800);
         },
