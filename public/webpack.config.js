3a55f42d43c1eff875f99661cb3e5bd907745231
朱重熊 <308083469@qq.com>
2018-01-12 17:21:02 +0800

    资源文件

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '/dist/',
+        publicPath: 'http://moodindex.oss-cn-shanghai.aliyuncs.com/wx/build/dist/',
         filename: 'build.js?[hash:8]', libraryTarget: 'umd',
         umdNamedDefine: true
     },
