4d0528528839db0a4ae6a1d2e333a17507508f39
朱重熊 <251675335@qq.com>
2018-01-14 19:22:45 +0800

    bug

--- public/src/component/Edit.vue
+++ public/src/component/Edit.vue
@@ -301,6 +301,7 @@
                 wx.getLocation({
                     type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
                     success: function (res) {
+                        console.log('getLocation')
                         var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
                         var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
                         var speed = res.speed; // 速度，以米/每秒计
@@ -308,6 +309,7 @@
 
                         that.showModule = 'position';
                         var latLng = new qq.maps.LatLng(latitude, longitude);
+                        console.log('latLng：'+latLng)
                         window['geocoder'].getAddress(latLng);
                     },
                     cancel: function (res) {
